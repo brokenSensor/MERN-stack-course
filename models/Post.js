b252b49f6e00f8922af1e39f64a730826c90eaf8
brokenSensor <jonimen95@yandex.ru>
2021-02-26 11:04:25 +0800

    Post model

---
+++ models/Post.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+
+const PostSchema = new mongoose.Schema({
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'users',
+	},
+	text: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+	},
+	avater: {
+		type: String,
+	},
+	likes: [
+		{
+			user: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'users',
+			},
+		},
+	],
+	comments: [
+		{
+			user: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'users',
+			},
+			text: {
+				type: String,
+				required: true,
+			},
+			avatar: {
+				type: String,
+			},
+			date: {
+				type: Date,
+				default: Date.now,
+			},
+		},
+	],
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+export default mongoose.model('post', PostSchema);
