a4c337dc803003db32b2ac96ebace519d8c84fe9
brokenSensor <jonimen95@yandex.ru>
2021-02-26 12:05:49 +0800

    Create post route

--- models/Post.js
+++ models/Post.js
@@ -12,7 +12,7 @@ const PostSchema = new mongoose.Schema({
 	name: {
 		type: String,
 	},
-	avater: {
+	avatar: {
 		type: String,
 	},
 	likes: [
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,41 @@
 import { Router } from 'express';
+import auth from '../../middleware/auth.js';
+import { check, validationResult } from 'express-validator';
+import User from '../../models/User.js';
+import Post from '../../models/Post.js';
+import Profile from '../../models/Profile.js';
 const router = Router();
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => {
-	res.send('Posts route');
-});
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+	'/',
+	[auth, [check('text', 'Text is required').not().isEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		try {
+			const user = await User.findById(req.user.id).select('-password');
+
+			const newPost = new Post({
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id,
+			});
+
+			const post = await newPost.save();
+
+			res.json(post);
+		} catch (error) {
+			console.error(error);
+			res.status(500).send('Server error');
+		}
+	}
+);
 
 export default router;
