eeacaf28b58a6ced4ea088e97e02bf8d7e600248
brokenSensor <jonimen95@yandex.ru>
2021-03-01 09:12:31 +0800

    Login action

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-function Login() {
+function Login({ login, isAuthenticated }) {
 	const [formData, setFormData] = useState({
 		email: '',
 		password: '',
@@ -15,8 +18,13 @@ function Login() {
 	const onSubmit = async e => {
 		e.preventDefault();
 
-		console.log('Yes');
+		login(email, password);
 	};
+
+	if (isAuthenticated) {
+		return <Redirect to='/dashboard' />;
+	}
+
 	return (
 		<>
 			<h1 className='large text-primary'>Sign In</h1>
@@ -54,4 +62,13 @@ function Login() {
 	);
 }
 
-export default Login;
+Login.propTypes = {
+	login: PropTypes.func.isRequired,
+	isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = state => ({
+	isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { login })(Login);
