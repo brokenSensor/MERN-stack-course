33eaa154fb533e4cc3dacc91a9013a7e7bc87d7a
brokenSensor <jonimen95@yandex.ru>
2021-03-01 15:57:09 +0800

    Dashboard and clear profie action

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
 
 const initialState = {
 	profile: null,
@@ -18,6 +18,9 @@ export default function (state = initialState, action) {
 
 		case PROFILE_ERROR:
 			return { ...state, error: payload, loading: false };
+
+		case CLEAR_PROFILE:
+			return { ...state, profile: null, repos: [], loading: false };
 
 		default:
 			return state;
