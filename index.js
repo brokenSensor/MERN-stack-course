c88a839e10f969e39f3be7a3fcf12e7531b048a7
brokenSensor <jonimen95@yandex.ru>
2021-03-03 14:06:38 +0800

    Pre deploy

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import usersRoute from './routes/api/users.js';
 import authRoute from './routes/api/auth.js';
 import postsRoute from './routes/api/posts.js';
 import profileRoute from './routes/api/profile.js';
+import path from 'path';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -15,15 +16,21 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-	res.send('API is Running');
-});
-
 // Define Routes
 app.use('/api/users', usersRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postsRoute);
 app.use('/api/profile', profileRoute);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	//Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
